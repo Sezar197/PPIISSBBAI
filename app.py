f4db39b71b84f6ca413229f79087b604ce6265b5
Sezar197 <33035926+Sezar197@users.noreply.github.com>
2026-04-19 19:17:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ import streamlit as st
 import numpy as np
 import joblib
 #from tensorflow.keras.models import load_model
-
+def timoshenko_deflection(E, nu, b, h, P, L=4000):
+    A = b * h
+    I = (b * h**3) / 12
+    G = E / (2 * (1 + nu))
+    k = 5/6
+    
+    delta_b = (P * L**3) / (48 * E * I)
+    delta_s = (P * L) / (4 * k * G * A)
+    
+    return delta_b + delta_s
 #rf = joblib.load("rf_model.pkl")
 xgb = joblib.load("xgb_model.pkl")
 scaler = joblib.load("scaler.pkl")
@@ -18,7 +27,11 @@ P = st.number_input("Load", value=1000000.0)
 
 if st.button("Predict"):
     X = np.array([[E, nu, w, d, P]])
+    # Analytical value
+    theo = timoshenko_deflection(E, nu, b, h, P)
 
+    # Error %
+    error = abs((xgb_pred - theo) / theo) * 100
     #rf_pred = rf.predict(X)[0]
     xgb_pred = xgb.predict(X)[0]
 
@@ -28,3 +41,5 @@ if st.button("Predict"):
     st.write("### Predictions")
     st.write(f"XGBoost: {xgb_pred}")
     #st.write(f"ANN: {ann_pred}")
+    st.write(f"Timoshenko (Analytical): {theo}")
+    st.write(f"Error (%): {error:.4f}")
