092bd6e1a77eae9a8a18599b977068d96f2d819c
Sezar197 <33035926+Sezar197@users.noreply.github.com>
2026-04-19 19:08:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import joblib
 #from tensorflow.keras.models import load_model
 
-rf = joblib.load("rf_model.pkl")
+#rf = joblib.load("rf_model.pkl")
 xgb = joblib.load("xgb_model.pkl")
 scaler = joblib.load("scaler.pkl")
 ann = load_model("ann_model.h5")
@@ -19,13 +19,12 @@ P = st.number_input("Load", value=1000000.0)
 if st.button("Predict"):
     X = np.array([[E, nu, w, d, P]])
 
-    rf_pred = rf.predict(X)[0]
+    #rf_pred = rf.predict(X)[0]
     xgb_pred = xgb.predict(X)[0]
 
     X_scaled = scaler.transform(X)
     ann_pred = ann.predict(X_scaled)[0][0]
 
     st.write("### Predictions")
-    st.write(f"Random Forest: {rf_pred}")
     st.write(f"XGBoost: {xgb_pred}")
     st.write(f"ANN: {ann_pred}")
